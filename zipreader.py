b577a7b540266ad87969712077bb2521a3c4a7d3
JuhaS <suomalainen.juha@gmail.com>
2012-01-03 23:04:14 +0200

    Update zipreader.py

--- zipreader.py
+++ zipreader.py
@@ -1,10 +1,10 @@
 import zipfile
 	
 def fileiterator(zipf):
-	with zipfile.ZipFile(zipf, "r", zipfile.ZIP_STORED) as z:
-		filelist = z.infolist()
+	with zipfile.ZipFile(zipf, "r", zipfile.ZIP_STORED) as openzip:
+		filelist = openzip.infolist()
 		for f in filelist:
-			yield(f.filename, z.read(f))
+			yield(f.filename, openzip.read(f))
 
 def process(zipf, callback):
 	with zipfile.ZipFile(zipf, "r", zipfile.ZIP_STORED) as openzip:
